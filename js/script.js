0c176fd3737541d27087e067e05a18e1f987a388
manuelmenama <manuel.mena090589@gmail.com>
2022-10-12 16:13:03 +0200

    restarted js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//data layer
+const userAge = 30;
+const kmRequest = 350;
+const agedDiscount = 0.4;
+const youngDiscount = 0.2;
+const oneKmPrice = 0.21;
+
+let outputMsg, totalPrice, discountedPrice;
+
+totalPrice = kmRequest * oneKmPrice
+
+//business layer
+
+//presentation layer
+console.log('prezzo pieno', totalPrice);
