70645126ff8d315027a824630159489e235ca1ec
manuelmenama <manuel.mena090589@gmail.com>
2022-10-12 16:23:34 +0200

    added logic

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //data layer
-const userAge = 30;
+const userAge = 17;
 const kmRequest = 350;
 const agedDiscount = 0.4;
 const youngDiscount = 0.2;
@@ -10,6 +10,13 @@ let outputMsg, totalPrice, discountedPrice;
 totalPrice = kmRequest * oneKmPrice
 
 //business layer
+if(userAge < 18){
+    discountedPrice = totalPrice * (1 - youngDiscount);
+    console.log('prezzo sconto young', discountedPrice.toFixed(2));
+}else if(userAge >= 65){
+    discountedPrice = totalPrice * (1 - agedDiscount);
+    console.log('prezzo sconto aged', discountedPrice.toFixed(2));
+}
 
 //presentation layer
 console.log('prezzo pieno', totalPrice);
