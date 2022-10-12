3a395d4b72804c274896a778a444f53cf2246a11
manuelmenama <manuel.mena090589@gmail.com>
2022-10-12 22:39:44 +0200

    completo senza stile

--- js/script.js
+++ js/script.js
@@ -1,28 +1,51 @@
 //data layer
-const userAge;
-const kmRequest = 350;
+
 const agedDiscount = 0.4;
 const youngDiscount = 0.2;
 const oneKmPrice = 0.21;
+let outputMsg, totalPrice, discountedPrice;
 
+let userAge;
+userAge = document.getElementById('selectDiscount').value;
+let kmRequest;
+kmRequest = document.getElementById('inputKm').value;
+let userName;
+userName = document.getElementById('inputName').value;
 
-let outputMsg, totalPrice, discountedPrice, name;
 
 totalPrice = kmRequest * oneKmPrice;
 
+//business layer
 const btnInput = document.getElementById('btnSubmit');
-btnInput.addEventListenerù('click', function(){
+btnInput.addEventListener('click', function(){
+    document.getElementById('nome').innerHTML = userName;
+    document.getElementById('offerta').innerHTML = userAge;
+    let carrozza;
+    carrozza =  Math.ceil(Math.random() * 10);
+    document.getElementById('carrozza').innerHTML = carrozza;
+    let codiceCP;
+    codiceCP = Math.floor(Math.random() * 100000);
+    document.getElementById('codice-CP').innerHTML = codiceCP;
 
+    if(userAge === "minorenne"){
+        discountedPrice = totalPrice * (1 - youngDiscount);
+        document.getElementById('offerta').innerHTML = `Utente minorenne`;
+        outputMsg = `<s>${totalPrice.toFixed(2)}</s>&euro; ${discountedPrice.toFixed(2)}&euro;`;
+        
+        console.log('prezzo sconto young', discountedPrice);
+    }else if(userAge === "over-65"){
+        discountedPrice = totalPrice * (1 - agedDiscount);
+        document.getElementById('offerta').innerHTML = `Utente senior`;
+        outputMsg = `<s>${totalPrice.toFixed(2)}</s>&euro; ${discountedPrice.toFixed(2)}&euro;`;
+    
+        console.log('prezzo sconto aged', discountedPrice);
+    }else{
+        document.getElementById('offerta').innerHTML = `Utente standard`;
+        outputMsg = `${totalPrice.toFixed(2)} &euro;`;
+    };
+    
+    //presentation layer
+    document.getElementById('costo-finale').innerHTML = outputMsg;
+    console.log('prezzo pieno', totalPrice);
 });
 
-//business layer
-if(userAge === "minorenne"){
-    discountedPrice = totalPrice * (1 - youngDiscount);
-    console.log('prezzo sconto young', discountedPrice.toFixed(2));
-}else if(userAge === "over65"){
-    discountedPrice = totalPrice * (1 - agedDiscount);
-    console.log('prezzo sconto aged', discountedPrice.toFixed(2));
-}
-
-//presentation layer
-console.log('prezzo pieno', totalPrice);
